07dec0768bdd1ebe07ab08f6618f693ff8176722
Aakash <thisisaakash9@gmail.com>
2026-03-28 10:31:37 -0400

    Add install/uninstall scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Lang uninstaller
-
-if [ -f "/usr/local/bin/lang" ]; then
-    sudo rm /usr/local/bin/lang
-    echo "Lang uninstalled."
-else
-    echo "Lang is not installed."
-fi
+sudo rm -f /usr/local/bin/lang
+echo "Lang uninstalled."
